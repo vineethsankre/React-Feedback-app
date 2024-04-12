ef53d8070aa25aed1cc6f5b8585f9a60508a9e0d
vineethsankre <lsvineethsankre@gmail.com>
2024-04-12 14:50:59 +0000

    render functions  separated

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -6,28 +6,51 @@ class Feedback extends Component {
     isFeedbackGiven = false,
   }
   onClickEmoji = () => this.setState({isFeedbackGiven: true})
-  render() {
+
+  renderFeedbackPage = () => {
     const {resources} = this.props
     const {emojis} = resources
     return (
+      <div className="feedback-container">
+        <h1 className="heading">
+          How satisfied are you with our customer support performance?
+        </h1>
+        <ul className="emoji-container">
+          {emojis.map(eachEmoji => (
+            <li className="emoji" key={eachEmoji.id}>
+              <img
+                className="emoji-img"
+                src={eachEmoji.imageUrl}
+                alt={eachEmoji.name}
+                onClick={this.onClickEmoji}
+              />
+              <p className="emoji-name">{eachEmoji.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+  renderThankYouScreen = () => {
+    const {resources} = this.props
+    const {loveEmojiUrl} = resources
+    return (
+      <div>
+        <img src= {loveEmojiUrl} alt="love emoji"/>
+        <h1>Thank You</h1>
+        <p>We will use your feedback to improve our customer support performance</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isFeedbackGiven} = this.state 
+    return (
       <div className="app-container">
-        <div className="feedback-container">
-          <h1 className="heading">
-            How satisfied are you with our customer support performance?
-          </h1>
-          <ul className="emoji-container">
-            {emojis.map(eachEmoji => (
-              <li className="emoji" key={eachEmoji.id}>
-                <img
-                  className="emoji-img"
-                  src={eachEmoji.imageUrl}
-                  alt={eachEmoji.name}
-                  onClick={this.onClickEmoji}
-                />
-                <p className="emoji-name">{eachEmoji.name}</p>
-              </li>
-            ))}
-          </ul>
+        <div>
+          {isFeedbackGiven
+            ? this.renderThankYouScreen()
+            : this.renderFeedbackPage()}
         </div>
       </div>
     )
